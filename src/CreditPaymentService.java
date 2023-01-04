5231b1a175d4c603e03cbd9c5df2710c9cfa2e78
Kratos410 <fanur.tt64@gmail.com>
2023-01-04 12:51:30 +0300

    added variable

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,16 +1,16 @@
 public class CreditPaymentService {
-    public double calculate(int loan, int term, double interestRate) {
+    public double  calculate(int loan, int term, double interestRate) {
         double m = (interestRate / 12) / 100; //m — процентная ставка в месяц, равная годовой ставке, поделённой на 12,
         double coefficient;  // m × (1 + m)ⁿ , n — количество платежей.
         if (term == 1) {
         coefficient =( m * ((1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m)))/(((1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m))-1);
-        }
-        if (term == 2) {
+        }else if (term == 2) {
         coefficient =(m * ((1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m)))/(((1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m))-1);
-            }
-        if (term == 3) {
+        }else if (term == 3) {
         coefficient =( m * ((1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m)))/((((1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m) * (1 + m)))-1);
-                }
+        }else{
+            coefficient = 0;
+        }
 
         double payments  = loan * coefficient;
         return payments;
--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,18 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         int loan = 1_000_000;           // Сумма кредита в рублях
-        int term = 1;                   // Срок кредита в годах
+        int term = 4;                   // Срок кредита в годах
         double interestRate = 9.99;     // Процентная ставка %
         double payments = service.calculate(loan, term, interestRate);    //Выплаты
         System.out.println("Сумма кредита: " + loan + " р.");
-        if (term == 1) {
+        if (term == 1){
             System.out.println("Срок кредита : " + term + " год");
-        }
-        if (term == 2) {
+        } else if (term == 2){
             System.out.println("Срок кредита : " + term + " года");
-        }
-        if (term == 3) {
+        }else if (term == 3){
             System.out.println("Срок кредита : " + term + " года");
+        } else if (payments == 0){
+            System.out.println("Простите, срок кредита должен составлять от 1 до 3 лет");
         }
         System.out.println("Процентная ставка: " + interestRate + " %");
         System.out.println("Ежемесячная выплата: " + payments + " р.");
